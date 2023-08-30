2323e46791412f63d87edb9278d5120069b201a4
MedaAkhil <medaakhilaeshchowdary@gmail.com>
2023-08-30 19:38:54 +0530

    calculation logic added

--- src/App.js
+++ src/App.js
@@ -9,7 +9,15 @@ function App() {
   const [coffeeType, setCoffeeType] = useState('');
   const [cupType,setCupType] = useState('');
   const costPerMl=1;
-
+  var coffeeCost = {
+    espresso:2,
+    cappuccino:9,
+    latte:3,
+    americano:5,
+    mocha:7,
+    macchiato:6,
+  }
+  const [netCost, setNetCost] = useState(''); 
   return (
     <div className="App">
       <header className="App-header">
@@ -25,6 +33,7 @@ function App() {
           <p>Select your favorite type of coffee:</p>
 
           <select id="coffeeDropdown" value={coffeeType} onChange={(v)=>setCoffeeType(v.target.value)}>
+          <option value="">Choose Coffee Type</option>
             <option value="espresso">Espresso</option>
             <option value="cappuccino">Cappuccino</option>
             <option value="latte">Latte</option>
@@ -32,6 +41,10 @@ function App() {
             <option value="mocha">Mocha</option>
             <option value="macchiato">Macchiato</option>
           </select>
+        </div>
+        <div>
+        <button className="redbut" onClick={()=> setNetCost(coffeeCost[coffeeType]*cupType)}><img className="pwbt" src={require('./images/power-on.png')}></img></button>
+          <br /><h4>Your Bill:{netCost}</h4>
         </div>
         <div className="bottompannelcup">
           <button value={cupType} onClick={() => setCupType(100)}><img className="img1" src={require('./images/coffee-cup.png')}></img></button>
