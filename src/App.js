b5735c4cb29facc2a0324c8082f06f6a4c34804a
MedaAkhil <medaakhilaeshchowdary@gmail.com>
2023-08-30 19:04:15 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,46 @@
-import logo from './logo.svg';
+import { 
+  BiCoffee,
+} from "react-icons/bi";
+import { useState } from "react";
+
 import './App.css';
 
 function App() {
+  const [coffeeType, setCoffeeType] = useState('');
+  const [cupType,setCupType] = useState('');
+  const costPerMl=1;
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p><BiCoffee />    Coffee Machine</p>
+        <p>Have a good coffee</p>
       </header>
+      <section className="coffeeshop">
+        <img src={require('./images/coffee-shop.png')}></img>
+      </section>
+      <section className="bottompannel">
+        <div className="bottompannelmenu">
+          <h1>Choose Your Coffee</h1>
+          <p>Select your favorite type of coffee:</p>
+
+          <select id="coffeeDropdown" value={coffeeType} onChange={(v)=>setCoffeeType(v.target.value)}>
+            <option value="espresso">Espresso</option>
+            <option value="cappuccino">Cappuccino</option>
+            <option value="latte">Latte</option>
+            <option value="americano">Americano</option>
+            <option value="mocha">Mocha</option>
+            <option value="macchiato">Macchiato</option>
+          </select>
+        </div>
+        <div className="bottompannelcup">
+          <button value={cupType} onClick={() => setCupType(100)}><img className="img1" src={require('./images/coffee-cup.png')}></img></button>
+          <button value={cupType} onClick={() => setCupType(200)}><img className="img2" src={require('./images/coffee-cup.png')}></img></button>
+          <button value={cupType} onClick={() => setCupType(300)}><img className="img3" src={require('./images/coffee-cup.png')}></img></button>
+        </div>
+      </section>
     </div>
+    
   );
 }
 
